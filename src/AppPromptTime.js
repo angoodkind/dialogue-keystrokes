100f3fc05591f20795be0f9de40c1d775f72292e
Adam <a.goodkind@gmail.com>
2021-11-04 11:20:35 -0500

    no firebase

--- src/AppPromptTime.js
+++ src/AppPromptTime.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import openSocket from 'socket.io-client';
 // import './App.css';
-import firebase from 'firebase/app';
-import 'firebase/database';
+// import firebase from 'firebase/app';
+// import 'firebase/database';
 import * as constants from './constants';
 
 // Must configure firebase before using its services
-firebase.initializeApp(constants.firebaseConfig);
+// firebase.initializeApp(constants.firebaseConfig);
 
 // Open a connection to the socket.io server 
-const socket = openSocket(`${constants.ec2Base}:8080`, {rejectUnauthorized: false, transports: ['websocket']});
+// const socket = openSocket(`${constants.ec2Base}:8080`, {rejectUnauthorized: false, transports: ['websocket']});
 
 // time-stamp at beginning of experiment
 const d = new Date();  
@@ -97,121 +97,121 @@ function AppPromptTime() {
 
  
   
-  // The keystrokes variable is how we will store the write location on keydowns
-  // and write to the same location on key ups.
-  const [keystrokes, setKeystrokes] = useState({});
+//   // The keystrokes variable is how we will store the write location on keydowns
+//   // and write to the same location on key ups.
+//   const [keystrokes, setKeystrokes] = useState({});
 
-  useEffect(() => {
-    window.onkeydown = async function (e) {
-      const info = {
-        "keyupordown": "down",
-        "eCode": e.code, 
-        "eKey": e.key, 
-        "eKeyCode": e.keyCode, 
-        "timestamp": Date.now(),
-        "existingTextMessage": message,
-        "visibleTextKeystroke": null
-      }
-      if (experiment != null) {
-        // Map the keystroke to its latest firebase node.
-        setKeystrokes(Object.assign(keystrokes, {[e.code]: firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject' +  subject + '/keys').push().key}));
-        // Write the info object to that location.
-        firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject'  + subject + '/keys/' + keystrokes[[e.code]]).push(info); 
-        console.log("After down: ", keystrokes)
-      }
-    }
-    window.onkeyup = async function (e) {
-      const info = {
-        "keyupordown": "up",
-        "eCode": e.code, 
-        "eKey": e.key, 
-        "eKeyCode": e.keyCode, 
-        "timestamp": Date.now(),
-        "existingTextMessage": message,
-        "visibleTextKeystroke": (e.key.length === 1 || e.code === "Backspace" ? e.key : null),
-      }
-      if (experiment != null) {
-        // Retrieve the latest firebase node for the given keystroke.
-        // Write the info object to that location.
+//   useEffect(() => {
+//     window.onkeydown = async function (e) {
+//       const info = {
+//         "keyupordown": "down",
+//         "eCode": e.code, 
+//         "eKey": e.key, 
+//         "eKeyCode": e.keyCode, 
+//         "timestamp": Date.now(),
+//         "existingTextMessage": message,
+//         "visibleTextKeystroke": null
+//       }
+//       if (experiment != null) {
+//         // Map the keystroke to its latest firebase node.
+//         setKeystrokes(Object.assign(keystrokes, {[e.code]: firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject' +  subject + '/keys').push().key}));
+//         // Write the info object to that location.
+//         firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject'  + subject + '/keys/' + keystrokes[[e.code]]).push(info); 
+//         console.log("After down: ", keystrokes)
+//       }
+//     }
+//     window.onkeyup = async function (e) {
+//       const info = {
+//         "keyupordown": "up",
+//         "eCode": e.code, 
+//         "eKey": e.key, 
+//         "eKeyCode": e.keyCode, 
+//         "timestamp": Date.now(),
+//         "existingTextMessage": message,
+//         "visibleTextKeystroke": (e.key.length === 1 || e.code === "Backspace" ? e.key : null),
+//       }
+//       if (experiment != null) {
+//         // Retrieve the latest firebase node for the given keystroke.
+//         // Write the info object to that location.
 
-        firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject'  +  subject + '/keys/' + keystrokes[[e.code]]).push(info).then(() => {
-          console.log("In the middle: ", keystrokes);
-          // Erase the association between the pressed key and specific firebase node
-          setKeystrokes(Object.assign(keystrokes, {[e.code]: null}));
-        }).then(() => {
-          console.log("After up: ", keystrokes);
-        })
-      }
-    }
-  })
-
-
-  useEffect(()=> {
-    if (sends != null && sends.from === subject) {
-      // "Sends" is an object storing the information for chats about to be sent. 
-      firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject' + subject + '/sends').push(sends)
-    }
-  },[sends])
-
-  useEffect(()=> {
-    if (subject === 1) {
-      // If the subject is the second person in the room (subject 1), get the current room number from the server
-      // So that both subjects write to the same location in firebase
-      let myKey = firebase.database().ref('prod').push().key;
-      socket.emit('setNode', {signal: myKey, room: room });
-    } else {
-      // If the subject is the first person in the room (subject 0), get a new room number that the next subject that
-      // enters the room can use.
-      socket.emit('getNode', {room: room});
-    }
-  },[subject, room])
+//         firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject'  +  subject + '/keys/' + keystrokes[[e.code]]).push(info).then(() => {
+//           console.log("In the middle: ", keystrokes);
+//           // Erase the association between the pressed key and specific firebase node
+//           setKeystrokes(Object.assign(keystrokes, {[e.code]: null}));
+//         }).then(() => {
+//           console.log("After up: ", keystrokes);
+//         })
+//       }
+//     }
+//   })
 
 
-  // When more messages than visible in the chat interface can be shown,
-  // The chat will automatically scroll to the latest chat on send / unless the user scrolls up
-  function updateScroll(){
-    var element = document.getElementById("messages");
-    element.scrollTop = element.scrollHeight;
-  }
+//   useEffect(()=> {
+//     if (sends != null && sends.from === subject) {
+//       // "Sends" is an object storing the information for chats about to be sent. 
+//       firebase.database().ref('prod/' + experiment + '/prompt' + constants.prompts[prompt].promptNum + '/subject' + subject + '/sends').push(sends)
+//     }
+//   },[sends])
 
-  useEffect(() => { 
-    if (subject != null) {
-      socket.on("message", (result) => {
-        console.log(`result.user: ${result.user}`);
-        console.log(`subject number: ${subject}`);
-        console.log("received: ", Date.now())
-        const data = {
-          "from": result.user,
-          "timeSent": sentTime,
-          "timeReceived": Date.now(),
-          "message": result.data
-        }
-        setSends(data);
-        // When the socket receives a message, it has to know if this message was sent by
-        // a different client or itself.
-        // Based on the identity of the sender it will render an appropriately styled chat box
-        // Controlled by CSS classes.
-        if (result.user === subject) {
-          console.log("same")
-          document.getElementById('messages').innerHTML += 
-          ` 
-            <div class="o-out band">
-              <div class="o-in message">${result.data}</div>
-            </div>
-          `
-        } else {
-          console.log("different")
-          document.getElementById('messages').innerHTML += 
-          ` 
-            <div class="m-out band">
-              <div class="m-in message">${result.data}</div>
-            </div>
-          `
-        }
-        updateScroll();
-      })
-    }
-  },[subject])
+//   useEffect(()=> {
+//     if (subject === 1) {
+//       // If the subject is the second person in the room (subject 1), get the current room number from the server
+//       // So that both subjects write to the same location in firebase
+//       let myKey = firebase.database().ref('prod').push().key;
+//       socket.emit('setNode', {signal: myKey, room: room });
+//     } else {
+//       // If the subject is the first person in the room (subject 0), get a new room number that the next subject that
+//       // enters the room can use.
+//       socket.emit('getNode', {room: room});
+//     }
+//   },[subject, room])
+
+
+//   // When more messages than visible in the chat interface can be shown,
+//   // The chat will automatically scroll to the latest chat on send / unless the user scrolls up
+//   function updateScroll(){
+//     var element = document.getElementById("messages");
+//     element.scrollTop = element.scrollHeight;
+//   }
+
+//   useEffect(() => { 
+//     if (subject != null) {
+//       socket.on("message", (result) => {
+//         console.log(`result.user: ${result.user}`);
+//         console.log(`subject number: ${subject}`);
+//         console.log("received: ", Date.now())
+//         const data = {
+//           "from": result.user,
+//           "timeSent": sentTime,
+//           "timeReceived": Date.now(),
+//           "message": result.data
+//         }
+//         setSends(data);
+//         // When the socket receives a message, it has to know if this message was sent by
+//         // a different client or itself.
+//         // Based on the identity of the sender it will render an appropriately styled chat box
+//         // Controlled by CSS classes.
+//         if (result.user === subject) {
+//           console.log("same")
+//           document.getElementById('messages').innerHTML += 
+//           ` 
+//             <div class="o-out band">
+//               <div class="o-in message">${result.data}</div>
+//             </div>
+//           `
+//         } else {
+//           console.log("different")
+//           document.getElementById('messages').innerHTML += 
+//           ` 
+//             <div class="m-out band">
+//               <div class="m-in message">${result.data}</div>
+//             </div>
+//           `
+//         }
+//         updateScroll();
+//       })
+//     }
+//   },[subject])
 
   useEffect(()=> {
     // This is the enter button that sends a message.
