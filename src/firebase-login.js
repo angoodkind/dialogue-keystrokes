eb9824f8cdaffe6a20198e402ae6c6e3a47a745f
Adam <a.goodkind@gmail.com>
2022-01-20 12:06:31 -0600

    update gitignore and fb credentials

---
+++ src/firebase-login.js
@@ -0,0 +1,12 @@
+
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyBWJmpNZvlexkgSRvCi9IGe4ZZFGgaE9sc",
+    authDomain: "keystroke-dialogue.firebaseapp.com",
+    projectId: "keystroke-dialogue",
+    storageBucket: "keystroke-dialogue.appspot.com",
+    messagingSenderId: "258869565916",
+    appId: "1:258869565916:web:a5388f1b8ec7ee25709719"
+  };
+
+export const ec2Base = 'http://ec2-18-223-160-60.us-east-2.compute.amazonaws.com'
